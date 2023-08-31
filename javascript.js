6a87b487e777d4cd42fd7b1ed18b8926d576232b
Ohm Patel <60417541+Ohm013@users.noreply.github.com>
2023-08-31 10:49:27 -0500

    query Selected all digits, operators, and container

---
+++ javascript.js
@@ -0,0 +1,6 @@
+//Issue #1 : Trying to create buttons 0-9 with loops so don't have to create them and querySelect all of them individually
+
+
+const container = document.querySelector("container");
+const digits = document.querySelectorAll("digits");
+
