2d1195be2ad5d88778ffc9524430976d565a3e98
Ohm Patel <60417541+Ohm013@users.noreply.github.com>
2023-09-01 15:08:33 -0500

    querySelected all the numbers and used forEach method to add event listenter to return the button value for each number

--- javascript.js
+++ javascript.js
@@ -1,6 +1,38 @@
 //Issue #1 : Trying to create buttons 0-9 with loops so don't have to create them and querySelect all of them individually
+// Solution #1 : Made the buttons manually
 
+//Issue #2: Trying to figure out how to get the add, subtract functions to recognize the buttons. Generalize it between a number and operator
 
 const container = document.querySelector("container");
-const digits = document.querySelectorAll("digits");
+const numbers = document.querySelectorAll(".digits .numbers");
+const operators = document.querySelectorAll("mathSigns"); 
+const num1 = document.querySelector('#num1');
 
+numbers.forEach((number) => {
+    number.addEventListener('click', function(e) {
+        console.log(e.target.value);
+       // return e.target.value
+});
+});
+
+
+//function mathOperation (numbers, operator) {
+  //  return `${numbers} + ${operator}`
+//}
+
+
+function add (num1, num2) { //prob will have to add reduce to these functions
+    return num1 + num2 ;
+}
+
+function subtract (num1, num2) { //prob will have to add reduce to these functions
+    return num1 - num2 ;
+}
+
+function multiply (num1, num2) { //prob will have to add reduce to these functions
+    return num1 * num2 ;
+}
+
+function divide (num1, num2) { //prob will have to add reduce to these functions
+    return num1 / num2 ;
+}
