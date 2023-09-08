e2399385018f0fa5e8f9fc888fc22d9987412f2e
Ohm Patel <60417541+Ohm013@users.noreply.github.com>
2023-09-08 14:23:20 -0500

    still trying to get it to chain multiple equations

--- javascript.js
+++ javascript.js
@@ -19,7 +19,7 @@
     // operate(operator);
 
   //They started as global variables so they can be accessed from anywhere. So I reassigned them in function operate like  
-      //function operate (num1,sign ,num2 ) {
+      //function operate (num1,sign ,num2 ) 
             //num1 = firstNum;
             //sign = operator ;
             //num2 = secondNum;
@@ -32,103 +32,106 @@
     // For ex: Trying to click 22 would mean press "2" once so that shows up then "2" again so it does "222". It adds the first 2 clicked to the 22 showing 222
 //Solution #5 : took out the "+" in "display.textContent  += `${char}`" 
 
+//Issue #6: Making the result of calculation show when operator clicked for a second time. When a operator is clicked a second time, it just changes it for the previous equation instead of showing th result and putting the second operator after the result number
+
+
 const container = document.querySelector("container");
 const numbers = document.querySelectorAll(".digits .numbers");
 const operators = document.querySelectorAll(".mathSigns"); 
 const display = document.querySelector('.display');
 const clear = document.querySelector("#clear");
 const del = document.querySelector("#delete");
+const equalSign = document.querySelector("#equal");
 
 let firstNum = "";
 let secondNum = "" ; 
 let operator = "";
-let result = "";
+let result = ""
 //if no operator then firstNum = first clicked and if there is operator then the clicked value = secondNum
 numbers.forEach((number) => {
     number.addEventListener('click', (e) => {
       
       if(operator === "") {
         firstNum += e.target.value ;
+        console.log(firstNum)
         operate(firstNum);
         screen(firstNum);
-      }else {
+      }else if( firstNum !== "") {
         secondNum += e.target.value; 
+        console.log(secondNum)
         operate(secondNum);
-        screen(secondNum); 
-        
-      } 
-     
+        screen(secondNum);  
+      }else if (operator == "equal") {
+        operate(firstNum,operator,secondNum); 
+       
+  
+ } })
     });
-   
-});
 
 
 operators.forEach((op => {
   op.addEventListener('click', (e) => {
-    if( op !== "=") {
+    op = operator
+    if( op === "") {
       operator =  e.target.value;
+      console.log(operator)
       screen(operator); 
       operate(operator);
-      //console.log(operator)
-   // }else if (op = e.target.value  && op === "equal") {
+    }else if (firstNum !== "" && secondNum !== ""){
+      operate(firstNum,secondNum);
+      
+      
+    }}
+)}));  
 
-   // }else if (operator === "+") {
-        
-     }
-   }) }))
- 
-
-  
 function operate (num1,sign ,num2 ) {
-num1 = Number(firstNum);
-sign = operator ;
-num2 = Number(secondNum);
-
-      switch(sign){
-        case "+": 
-          result = add(num1,num2)
-          break;
-        case "-": 
-        result = subtract(num1,num2)
-          break;
-        case "x": 
+  num1 = Number(firstNum);
+  sign = operator ;
+  num2 = Number(secondNum);
+  
+  switch(sign){  
+      case "+": 
+        result = add(num1,num2)
+        break;
+     case "-": 
+        result =subtract(num1,num2)
+        break;
+     case "x": 
         result = multiply(num1,num2)
-          break;
-        case "/": 
+        break;
+      case "/": 
         result = divide(num1,num2)
-          break;
-      }
-      console.log(result);
+        break;
+  }
+       console.log(result); 
+       screen(result); 
+}
+//function equalPressed () {
+  //equalSign.addEventListener('click', operate)
 
+//}
+function screen () {
+  num = firstNum ;
+    if (result !== ""){
+      display.textContent = `${result}`;
+    }
+  sign = operator
+  secNum = secondNum;
+  
+  display.textContent  = `${num}${sign}${secNum} `
+    //if (operator === "undefined")
 }
 
-clear.addEventListener('click', (e) => {
-  display.textContent = ""
-  firstNum  = ""
-  secondNum = ""
-  operator = ""
-  result = "";
-});
-
-function screen (a,b,c) {
-a = firstNum
-b = operator
-c= secondNum 
-
-    display.textContent  = `${a}${b}${c} `
-        if (typeof b === "undefined") {
-          return a
-        }else if(typeof b !== "undefined") {
-          return a + b
-        }else if(typeof c !== "undefined") {
-          return a + b + c
-        }else{
-          return 
-        }
+function clearAll () {
+  clear.addEventListener('click', () => {
+    display.textContent = ""
+    firstNum  = "";
+    secondNum = "";
+    operator = ""
+    result = "";
+  });
 }
-
-
-
+clearAll() ;
 
 function add (num1, num2) { //prob will have to add reduce to these functions
   return num1 + num2 ;
@@ -143,5 +146,5 @@ function multiply (num1, num2) { //prob will have to add reduce to these functio
 }
 
 function divide (num1, num2) { //prob will have to add reduce to these functions
-    return num1 / num2 ;
+  return num1 / num2 ;
 }
